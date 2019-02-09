5c0e4f5b8efaf7f491b0ca788e27d6a8c649ee2c
Trie0037 <trie0037@gmail.com>
2019-02-08 21:02:32 -0600

    minusid

--- spotify.js
+++ spotify.js
@@ -1,8 +1,7 @@
 var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify({
-    id: "8c0591a3af0c4e4085f51460f3d85c55",
-    secret: "2a7ca24916344620bb1e0e039329796a"
+    
 });
 
 module.exports = spotify;
